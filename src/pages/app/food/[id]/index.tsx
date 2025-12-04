3f861d7ab976e3f8e7d711216e82affedf2c7099
destiny <desto4q@gmail.com>
2025-12-04 16:20:10 +0100

    page breaking

--- src/pages/app/food/[id]/index.tsx
+++ src/pages/app/food/[id]/index.tsx
@@ -48,32 +48,6 @@ export default function index() {
     },
   });
 
-  if (query.isLoading) {
-    return <SimpleLoader />;
-  }
-  if (query.isError) {
-    return (
-      <div className="alert alert-error text-xl shadow-lg m-4">
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="stroke-current flex-shrink-0 h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-          <span>Error loading food item.</span>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <SuspensePageLayout query={query} showTitle={false}>
       {(data) => {

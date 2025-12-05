40ecc9b77f671a79d3e0ca46ca6c7f17231a44c6
destiny <desto4q@gmail.com>
2025-12-05 12:16:10 +0100

    status update food order

--- src/pages/app/food/orders/index.tsx
+++ src/pages/app/food/orders/index.tsx
@@ -5,15 +5,33 @@ import SuspensePageLayout from "@/components/layout/SuspensePageLayout";
 import SimplePaginator from "@/components/SimplePaginator";
 import SimpleTitle from "@/components/SimpleTitle";
 import CustomTable from "@/components/tables/CustomTable";
+import { extract_message } from "@/helpers/auth";
 import { usePagination } from "@/store/pagination";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { toast } from "sonner";
 const status_list = [
   "delivered",
   "cancelled",
   "preparing",
   "confirmed",
 ] as const;
+
+const allowed_Status_update = (
+  currentStatus: (typeof status_list)[number],
+): (typeof status_list)[number][] => {
+  switch (currentStatus) {
+    case "confirmed":
+      return ["preparing", "cancelled"];
+    case "preparing":
+      return ["delivered", "cancelled"];
+    case "delivered":
+    case "cancelled":
+      return []; // No further updates allowed for delivered or cancelled orders
+    default:
+      return [];
+  }
+};
 export default function index() {
   const [status, setStatus] =
     useState<(typeof status_list)[number]>("delivered");
@@ -30,6 +48,32 @@ export default function index() {
       return resp.data;
     },
   });
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: (fn: any) => fn(),
+    onSuccess: () => {
+      query.refetch();
+    },
+  });
+  const update_status = async (
+    id: string,
+    newStatus: (typeof status_list)[number],
+  ) => {
+    let resp = await apiClient.patch(`admins/orders/foods/${id}/status`, {
+      status: newStatus,
+    });
+    return resp.data;
+  };
+
+  const call_ = async (fn: () => Promise<any>) => {
+    toast.promise(
+      mutateAsync(() => fn()),
+      {
+        loading: "loading",
+        error: extract_message,
+        success: extract_message,
+      },
+    );
+  };
   return (
     <>
       <SimpleTitle title="Food Orders" />
@@ -86,6 +130,15 @@ export default function index() {
                     },
                   },
                 ]}
+                actions={status_list
+                  .filter((s) => allowed_Status_update(status).includes(s))
+                  .map((s) => ({
+                    key: s,
+                    label: `Mark as ${s}`,
+                    action: (item: FoodOrder) => {
+                      call_(() => update_status(item.id, s));
+                    },
+                  }))}
               />
               <EmptyList list={payload} />
               <div className="mt-4">

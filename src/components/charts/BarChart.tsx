cc5c3f5fdab55109932df4444fcd6a5092c96b74
destiny <desto4q@gmail.com>
2025-12-11 04:08:27 +0100

    anaylsis and responsiveness

--- src/components/charts/BarChart.tsx
+++ src/components/charts/BarChart.tsx
@@ -9,6 +9,7 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  ResponsiveContainer,
 } from "recharts";
 
 const SimpleBarChart = () => {
@@ -32,40 +33,42 @@ const SimpleBarChart = () => {
       : dummyData;
 
   return (
-    <div className="h-full   p-4 bg-base-100 shadow ring rounded-md ring-current/20">
+    <div className="h-full flex flex-col   p-4 bg-base-100 shadow ring rounded-md ring-current/20">
       <h2 className="mb-2 text-2xl font-bold">Income Analysis</h2>
-      <BarChart
-        style={{
-          width: "100%",
-          maxWidth: "700px",
-          height: "100%",
-          aspectRatio: 1.618,
-        }}
-        responsive
-        data={data}
-        margin={{
-          top: 5,
-          right: 0,
-          left: 0,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis width="auto" />
-        <Tooltip />
-        <Legend />
-        <Bar
-          dataKey="income"
-          fill="#8884d8"
-          activeBar={<Rectangle fill="pink" stroke="blue" />}
-        />
-        <Bar
-          dataKey="expense"
-          fill="#82ca9d"
-          activeBar={<Rectangle fill="gold" stroke="purple" />}
-        />
-      </BarChart>
+      <ResponsiveContainer style={{ flex: 1 }} className={""}>
+        <BarChart
+          // style={{
+          //   width: "100%",
+          //   maxWidth: "700px",
+          //   height: "100%",
+          //   aspectRatio: 1.618,
+          // }}
+          responsive
+          data={data}
+          margin={{
+            top: 5,
+            right: 0,
+            left: 0,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis width="auto" />
+          <Tooltip />
+          <Legend />
+          <Bar
+            dataKey="income"
+            fill="#8884d8"
+            activeBar={<Rectangle fill="pink" stroke="blue" />}
+          />
+          <Bar
+            dataKey="expense"
+            fill="#82ca9d"
+            activeBar={<Rectangle fill="gold" stroke="purple" />}
+          />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 };

b92fa71f9db439c8021fea32d983e55f2e8f50e6
destiny <desto4q@gmail.com>
2025-12-15 10:21:27 +0100

    search equipment rentals

--- src/pages/app/equipment/index.tsx
+++ src/pages/app/equipment/index.tsx
@@ -9,14 +9,23 @@ import { useQuery } from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 import EquipmentCard from "./_components/EquipmentCard";
+import { useSearchParams } from "@/helpers/client";
+import SimpleSearch from "@/components/SimpleSearch";
+import SuspensePageLayout from "@/components/layout/SuspensePageLayout";
+import SimpleHeader from "@/components/SimpleHeader";
 
 export default function index() {
   const [cat, setCat] = useState(null);
+  const searchProps = useSearchParams();
   const props = usePagination();
   const query = useQuery<ApiResponse<RentalEquipment[]>>({
-    queryKey: ["vr", props.page, cat],
+    queryKey: ["vr", props.page, cat, searchProps.search],
     queryFn: async () => {
-      const params: Record<string, any> = { page: props.page, categoryId: cat };
+      const params: Record<string, any> = {
+        page: props.page,
+        categoryId: cat,
+        search: searchProps.search,
+      };
       // only include categoryId if cat is not null
       // remove null/undefined keys from params
       Object.keys(params).forEach((key) => {
@@ -34,10 +43,8 @@ export default function index() {
   }, [cat]);
   const item = query.data?.payload;
   return (
-    <QueryPageLayout
-      query={query}
-      title={"Equipment Rental Items"}
-      headerActions={
+    <>
+      <SimpleHeader title={"Equipment Rental Items"}>
         <>
           <Link href="new" className="btn btn-primary">
             Add Equipment
@@ -55,16 +62,42 @@ export default function index() {
             }}
           />
         </>
-      }
-    >
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-2">
-        {item?.map((itm) => (
-          <EquipmentCard refetch={query.refetch} itm={itm} key={itm.id} />
-        ))}
-      </div>
-      <div className="mt-4">
-        <SimplePaginator {...props} />
-      </div>
-    </QueryPageLayout>
+      </SimpleHeader>
+      <SimpleSearch props={searchProps} />
+
+      <SuspensePageLayout
+        query={query}
+        showTitle={false}
+        title={"Equipment Rental Items"}
+        headerActions={
+          <>
+            <Link href="new" className="btn btn-primary">
+              Add Equipment
+            </Link>
+            <SimpleSelect
+              onChange={setCat}
+              route="admins/equipments/categories"
+              value={String(cat)}
+              render={(item: { name: string; id: string }) => {
+                return (
+                  <>
+                    <option value={item.id}>{item.name}</option>
+                  </>
+                );
+              }}
+            />
+          </>
+        }
+      >
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-2">
+          {item?.map((itm) => (
+            <EquipmentCard refetch={query.refetch} itm={itm} key={itm.id} />
+          ))}
+        </div>
+        <div className="mt-4">
+          <SimplePaginator {...props} />
+        </div>
+      </SuspensePageLayout>
+    </>
   );
 }

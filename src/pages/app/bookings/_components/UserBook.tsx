53968ef4e49b414ca086b3eff28ae2e848e17cc9
destiny <desto4q@gmail.com>
2025-12-16 14:49:55 +0100

    studio booking usertype

--- src/pages/app/bookings/_components/UserBook.tsx
+++ src/pages/app/bookings/_components/UserBook.tsx
@@ -2,14 +2,30 @@ import type { BookUser } from "@/api/types";
 
 export default function UserBook({ item }: { item: BookUser }) {
   return (
-    <div className="p-2 bg-base-300 ring ring-current/20 rounded-box  mt-auto">
-      {/*<div className=" fieldset-label ">User</div>*/}
-
-      <div>
-        <p className="font-semibold text-base">{`${item.firstName} ${item.lastName}`}</p>
-        <div className="opacity-80">
-          <p className="text-info text-sm">{item.email}</p>
-          <p className="text-info text-sm">{item.phone}</p>
+    <div className="card bg-base-200 shadow-md p-4 rounded-box mt-4">
+      <div className="flex items-center space-x-4">
+        {/*<div className="flex-shrink-0">
+          <div className="avatar">
+            <div className="w-10 rounded-full">
+              <img
+                src={
+                  item.profilePicture?.url || "https://via.placeholder.com/50"
+                }
+                alt="User Avatar"
+              />
+            </div>
+          </div>
+        </div>*/}
+        <div>
+          <p className="font-semibold text-base">{`${item.firstName} ${item.lastName}`}</p>
+          <div className="opacity-80">
+            <p className="text-sm">
+              <span className="font-medium">Email:</span> {item.email}
+            </p>
+            <p className="text-sm">
+              <span className="font-medium">Phone:</span> {item.phone}
+            </p>
+          </div>
         </div>
       </div>
     </div>

02a43d8b391776e034f43d40ea01ba857466a471
destiny <desto4q@gmail.com>
2025-12-16 14:45:07 +0100

    movie booking user type

--- src/pages/app/bookings/_components/UserBook.tsx
+++ src/pages/app/bookings/_components/UserBook.tsx
@@ -3,10 +3,10 @@ import type { BookUser } from "@/api/types";
 export default function UserBook({ item }: { item: BookUser }) {
   return (
     <div className="p-2 bg-base-300 ring ring-current/20 rounded-box  mt-auto">
-      <div className=" fieldset-label ">User</div>
+      {/*<div className=" fieldset-label ">User</div>*/}
 
       <div>
-        <p className="font-semibold text-sm">{`${item.firstName} ${item.lastName}`}</p>
+        <p className="font-semibold text-base">{`${item.firstName} ${item.lastName}`}</p>
         <div className="opacity-80">
           <p className="text-info text-sm">{item.email}</p>
           <p className="text-info text-sm">{item.phone}</p>
